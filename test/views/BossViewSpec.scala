f9ea0e163b8b31231ff9c5ce0b6024f38cb1d591
lewishayqa <lewis.hay@qa.com>
2018-01-27 15:40:37 +0000

    Updated view specs to test for more content

--- test/views/BossViewSpec.scala
+++ test/views/BossViewSpec.scala
@@ -10,6 +10,12 @@ class BossViewSpec extends ViewBaseSpec {
     val title = "h1"
     val formLabel = ".big-form > dl > dt > label"
     val formButton = ".big-form > button"
+    val bossName = "p:nth-child(1) > b:nth-child(1)"
+    val bossDescription = "p:nth-child(2) > b:nth-child(1)"
+    val bossHealth = "p:nth-child(3) > b:nth-child(1)"
+    val bossLevel = "p:nth-child(4) > b:nth-child(1)"
+    val zoneName = "p:nth-child(5) > b:nth-child(1)"
+    val zoneLocation = "p:nth-child(6) > b:nth-child(1)"
   }
 
   lazy val view = views.html.boss(Common.exampleBoss)
@@ -27,6 +33,17 @@ class BossViewSpec extends ViewBaseSpec {
 
     "have the correct button text" in {
       elementText(Selectors.formButton) shouldBe "Get boss!"
+    }
+
+    "have the correct boss field headings" in {
+      val bossFieldSelectors = Array(Selectors.bossName, Selectors.bossDescription, Selectors.bossHealth,
+        Selectors.bossLevel, Selectors.zoneName, Selectors.zoneLocation)
+
+      val expectedValues = Array("Name:", "Description:", "Health:", "Level:", "Zone name:", "Zone location:")
+
+      for((fieldValue, expectedValue) <- bossFieldSelectors.zip(expectedValues)) {
+        elementText(fieldValue) shouldBe expectedValue
+      }
     }
   }
 }
---
+++ test/views/PixelGridViewSpec.scala
@@ -0,0 +1,47 @@
+package views
+
+import common.{Common, Grids}
+import org.jsoup.Jsoup
+import org.jsoup.nodes.Document
+
+class PixelGridViewSpec extends ViewBaseSpec {
+
+  object Selectors {
+    val title = "h1"
+    val checkedGridButton = ".big-form:nth-child(2) > button:nth-child(1)"
+    val scalaGridButton = ".big-form:nth-child(4) > button:nth-child(1)"
+    val oneUpGridButton = ".big-form:nth-child(6) > button:nth-child(1)"
+    val corruptionGridButton = ".big-form:nth-child(8) > button:nth-child(1)"
+    val exampleGridPixel = ".fixed > tbody:nth-child(1) > tr:nth-child(4) > th:nth-child(8)"
+  }
+
+  lazy val view = views.html.pixelGrid(Common.gridList, Grids.scalaClass.name)
+  lazy implicit val document: Document = Jsoup.parse(view.body)
+
+  "The Pixel Grid page" should {
+
+    "have the correct title" in {
+      elementText(Selectors.title) shouldBe "Pixel grid"
+    }
+
+    "have the correct text on the checked grid button" in {
+      elementText(Selectors.checkedGridButton) shouldBe "Checked grid"
+    }
+
+    "have the correct text on the scala grid button" in {
+      elementText(Selectors.scalaGridButton) shouldBe "Scala class"
+    }
+
+    "have the correct text on the 1-up mushroom grid button" in {
+      elementText(Selectors.oneUpGridButton) shouldBe "1-up mushroom"
+    }
+
+    "have the correct text on the corruption grid button" in {
+      elementText(Selectors.corruptionGridButton) shouldBe "Corruption"
+    }
+
+    "have the correct image in the grid" in {
+      element(Selectors.exampleGridPixel).attr("bgcolor") shouldBe "#A3D6F2"
+    }
+  }
+}
